3d859f983fab3d4e93d1edd896303080cd24ef6a
Denis <denis.were@student.moringaschool.com>
2023-05-20 09:58:32 +0300

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,25 @@
+class Book
+    attr_reader :title, :author, :page_count, :genre
+  
+    def initialize(title)
+      @title = title
+    end
+  
+    def turn_page
+      puts "Flipping the page...wow, you read fast!"
+    end
+  end
 
+  book = Book.new("The Hitchhiker's Guide to the Galaxy")
+  puts book.title
 
+  book.author = "Douglas Adams"
+  puts book.author
+
+  book.page_count = 150
+  puts book.page_count
+
+  book.genre = "Sci-Fi"
+  puts book.genre
+
+  book.turn_page
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,44 @@
-# Make your shoe class here!
+class Shoe
+    attr_reader :brand, :color, :size, :material, :condition
+  
+    def initialize(brand)
+      @brand = brand
+      @condition = "new"
+    end
+  
+    def cobble
+      puts "Your shoe is as good as new!"
+      @condition = "new"
+    end
+  end
+  shoe = Shoe.new("Nike")
+
+puts shoe.brand
+
+
+shoe.color = "red"
+
+puts shoe.color
+
+
+shoe.size = 9.5
+
+puts shoe.size
+
+
+shoe.material = "suede"
+
+puts shoe.material
+
+
+shoe.condition = "tattered"
+
+puts shoe.condition
+
+
+shoe.cobble
+
+
+
+puts shoe.condition
+
